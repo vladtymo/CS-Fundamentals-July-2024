9d3e68c7b859dd656faa1d10db1d30d47ad4bc41
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-08-06 18:55:24 +0300

    Delegates example

---
+++ 09_delegates/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Threading.Channels;
+
+namespace _11_delegates
+{
+    delegate double Operation(double a, double b);
+    delegate T Modificator<T>(T value);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------- Delegates --------------");
+
+            Operation? op = Add;
+            op += Div;
+            //op = null;
+
+            op?.Invoke(1, 1); // null skip
+            op(1, 1);
+
+            Console.WriteLine("Result: " + op.Invoke(20, 7));
+
+            // Delegates is a MulticastDelegate
+            // [+=] - add new method reference
+            // [-=] - remove a method reference
+
+            op += Mult;
+            op += Sub;
+            op -= Mult;
+
+            Console.WriteLine("Result: " + op(20, 7)); // Sub result
+
+            // array of delegates
+            Operation[] operations = { Add, Sub, Mult, Div };
+
+            Console.Write("Enter A = ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter B = ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Operations:\n" +
+                "1 - Add\n" +
+                "2 - Substract\n" +
+                "3 - Multiply\n" +
+                "4 - Division");
+
+            Console.Write("Enter operation: ");
+            int action = int.Parse(Console.ReadLine());
+
+            var result = operations[action - 1].Invoke(a, b);
+            Console.WriteLine("Your operation result: " + result);
+
+            // ------------------- callbacks
+            int[] numbers = [1, 5, 2, 5, -1, 99, 120, -66, 5];
+
+            ChangeArray(numbers, Increment);
+            ChangeArray(numbers, Negative);
+
+            ChangeArray(numbers, delegate (int n) { return n * n; });
+            ChangeArray(numbers, (n) => n + n);
+
+            foreach (var i in numbers) Console.Write(i + " "); Console.WriteLine();
+
+            // ----------- Built-in Delegates: Action | Predicate | Func
+            // Action - can has input parameters but void return type
+            Action greeting = () => Console.WriteLine("Hello");
+            Action<string> greeting2 = (login) => Console.WriteLine("Hello, dear " + login);
+
+            // Func - has return type
+            Func<double, double, double> func = Mult;
+
+            // Predicate - returns boolean type
+            Predicate<int> isEven = (x) => x % 2 == 0;
+
+            greeting();
+            greeting2("vladtymo");
+            Console.WriteLine(func(4, 4));
+            Console.WriteLine(isEven(6));
+        }
+
+        static void ChangeArray(int[] arr, Modificator<int> modificator) // modificator - callback method
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = modificator(arr[i]);
+            }
+        }
+
+        static int Increment(int n) => n + 1;
+        static int Negative(int n) => n * -1;
+
+        static double Add(double a, double b)
+        {
+            Console.WriteLine("Adding numbers");
+            return a + b;
+        }
+        static double Sub(double a, double b) => a - b;
+        static double Mult(double a, double b) => a * b;
+        static double Div(double a, double b) => a / b;
+    }
+}
---
+++ 09_events/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace _11_events
+{
+    public class Car
+    {
+        public const int SPEED_LIMIT = 220;
+        public const int CRASH_DETECTION_DIF = 30;
+
+        private static Random random = new Random();
+        private float speed;
+
+        public event Action<float> CrashEvent;
+
+        public float Speed
+        {
+            get { return speed; }
+            set
+            {
+                // ------------- Crash Detection Logic
+                // speed - before change
+                // value - after change
+                if (Math.Abs(speed - value) >= CRASH_DETECTION_DIF)
+                    CrashEvent?.Invoke(speed); // invoke crash event
+                else
+                    speed = value;
+            }
+        }
+
+        public string Model { get; set; }
+        public string VIN { get; set; }
+
+        public Car(string vin, string model)
+        {
+            Model = model;
+            VIN = vin;
+            Speed = 0;
+        }
+
+        public void Gas()
+        {
+            float increase = random.Next(10);
+
+            if (Speed + increase <= SPEED_LIMIT)
+                Speed += increase;
+            else
+                Speed = SPEED_LIMIT;
+        }
+
+        public void Stop()
+        {
+            float decrease = random.Next(20);
+
+            if (Speed - decrease >= 0)
+                Speed -= decrease;
+            else
+                Speed = 0;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Car: {Model} current speed: {Speed} km/h";
+        }
+    }
+
+    internal class Program
+    {
+        static void Crash(Car car) => car.Speed = 0; // setter
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------- Events --------------");
+
+            Car myCar = new("GJR787EBR", "Nissan GT-R");
+
+            // [+=] - subscribe to the event
+            myCar.CrashEvent += CrashAlert;
+            myCar.CrashEvent += (s) => CallPolice();
+            myCar.CrashEvent += YourCar_CrashEvent;
+
+            // [-=] - unsubscribe from the event
+            myCar.CrashEvent -= CrashAlert;
+
+            // event provive only [-=] [+=] operators on public
+            //myCar.CrashEvent = null;
+            //myCar.CrashEvent.Invoke(0);
+
+            for (int i = 0; i < 20; i++)
+            {
+                myCar.Gas();
+
+                if (i % 7 == 0) Crash(myCar);
+            }
+            Console.WriteLine(myCar);
+
+            // -------------- another car
+            Car yourCar = new("VXG5677KLO", "Audi A8");
+            yourCar.CrashEvent += YourCar_CrashEvent;
+
+            Crash(yourCar);
+            Console.WriteLine(yourCar);
+        }
+
+        private static void YourCar_CrashEvent(float speed)
+        {
+            File.AppendAllText("crash_logs.txt", $"Crash Detected: {DateTime.Now} - {speed} km/h");
+        }
+
+        static void CrashAlert(float speed)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"Crash Detected on {speed} km/h");
+            Console.ResetColor();
+        }
+        static void CallPolice()
+        {
+            Console.WriteLine("Calling 911...");
+        }
+    }
+}
